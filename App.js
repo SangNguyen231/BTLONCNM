e7d0396a1cb5b94133e43360d5e403aedc9de39d
SangNguyen <ntpsang231@gmail.com>
2022-10-17 15:50:37 +0700

    update mobile app

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
+
+import { NavigationAction, NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Onboarding from './components/Onboarding';
+import DangNhap from './screens/DangNhap';
+import DangKy from './screens/DangKy';
+import QuenMatKhau from './screens/QuenMatKhau';
+import ResetMatKHau from './screens/ResetMatKhau';
+import NhapMaOTP from './screens/NhapMaOTP'
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name='Home' component={Onboarding}></Stack.Screen>
+        <Stack.Screen name='DangNhap' component={DangNhap}></Stack.Screen>
+        <Stack.Screen name='DangKy' component={DangKy}></Stack.Screen>
+        <Stack.Screen name='QuenMatKhau' component={QuenMatKhau}></Stack.Screen>
+        <Stack.Screen name='ResetMatKhau' component={ResetMatKHau}></Stack.Screen>
+        <Stack.Screen name='NhapMaOTP' component={NhapMaOTP}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
